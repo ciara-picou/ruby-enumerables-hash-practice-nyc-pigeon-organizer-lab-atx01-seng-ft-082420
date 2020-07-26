d7512cad36ca5468f88d334278aba100c05dc9fc
Ciara Picou <ciarapicou@att.net>
2020-07-26 02:44:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+
+  
+$pigeon_array = []
+$pigeon_list = Hash.new
+
+def create_pigeon_name_hash(data)
+data.each do |pigeon_info_hash , pigeon_info_details_hash|
+  
+    pigeon_info_details_hash.each do|info_key , value_array|
+      value_array.each do |pigeon_name|
+          $pigeon_array.push(pigeon_name)
+     end
+  end
+end
+$pigeon_array.uniq
+$pigeon_list = Hash[$pigeon_array.collect{|item| [item, "" ]}]
+$pigeon_list
+
+end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  create_pigeon_name_hash(data)
+  $pigeon_list.map do |lil_pidgies,values|
+    values = Hash[data.keys.collect { |values| [values, ""] } ]
+ 
+  end
+  $pigeon_list
 end
