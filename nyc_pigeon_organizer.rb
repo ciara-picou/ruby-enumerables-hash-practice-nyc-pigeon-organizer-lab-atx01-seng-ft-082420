a51cb7475f12b6fcd6217c313a26259e92cc8409
Ciara Picou <ciarapicou@att.net>
2020-07-28 20:08:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,56 +1,8 @@
 
-  
-$pigeon_array = []
-$pigeon_list = Hash.new
-
-def create_pigeon_name_hash(data)
-data.each do |pigeon_info_hash , pigeon_info_details_hash|
-  
-    pigeon_info_details_hash.each do|info_key , value_array|
-      value_array.each do |pigeon_name|
-          $pigeon_array.push(pigeon_name)
-     end
-  end
-end
-$pigeon_array.uniq!
-$pigeon_list = Hash[$pigeon_array.collect{|item| [item, {} ]}]
-$pigeon_list
-
-end
-
-def nyc_pigeon_organizer(data)
-  create_pigeon_name_hash(data)
-  $pigeon_list.map do |lil_pidgies,values|
-    values_hash = Hash[data.keys.collect { |values| [values, {}] } ]
-  
-# lil_pidgies.each_with_object)({}) do ||
-  end
-  $pigeon_list
-end
-
-# def nyc_pigeon_organizer(data)
-#   final_results = data.each_with_object({}) do |(key, value), final_array|
-    
-#value.each do |inner_key, names|
-#       names.each do |name|
-#         if !final_array[name]
-#           final_array[name] = {}
-           
-#         end
-#       if !final_array[name][key]
-#         !final_array[name][key] = []
-  
-#       end
-#     final_array[name][key].push(inner_key.to_s)
-#       end
-#     end
-#   end
-  
-# end
 
 
 
-# to create a new hash from an old one using map
+  # to create a new hash from an old one using map
 # hash = {a: 2, b: 3}
 # hash.map { |k, v| [k, v + 3] }.to_h     # => { a => 5, b => 6 }
 
