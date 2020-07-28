36219f0fb840f96e306f745ebcf35bea43dc0801
Ciara Picou <ciarapicou@att.net>
2020-07-28 17:46:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ data.each do |pigeon_info_hash , pigeon_info_details_hash|
   end
 end
 $pigeon_array.uniq!
-$pigeon_list = Hash[$pigeon_array.collect{|item| [item, "" ]}]
+$pigeon_list = Hash[$pigeon_array.collect{|item| [item, {} ]}]
 $pigeon_list
 binding.pry
 end
