2e3d9b1b4fc6002820211e6cb8f29a9c46c611a6
Ciara Picou <ciarapicou@att.net>
2020-07-28 20:37:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
       # to create a new hash from an old one using map
 # hash = {a: 2, b: 3}
 # hash.map { |k, v| [k, v + 3] }.to_h     # => { a => 5, b => 6 }
- pigeon_list = [pigeon_info_category_hash, pigeon_info_hash].to_h
+ pigeon_list = {}
  data.map  do |pigeon_info_category_hash, pigeon_info_hash|
  
 
