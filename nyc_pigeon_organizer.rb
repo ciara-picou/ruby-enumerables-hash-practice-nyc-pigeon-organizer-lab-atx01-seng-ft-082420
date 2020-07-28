4dc07f73e0783e8ed01da50900e9b722ec43a409
Ciara Picou <ciarapicou@att.net>
2020-07-28 20:43:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,7 @@
 
 def nyc_pigeon_organizer(data)
-  # final_results = data.each_with_object({}) do |(pigeon_info_category_hash, pigeon_info_hash), pigeon_list|
-     
-      # to create a new hash from an old one using map
-# hash = {a: 2, b: 3}
-# hash.map { |k, v| [k, v + 3] }.to_h     # => { a => 5, b => 6 }
- pigeon_list = {}
+   pigeon_list = {}
  data.map  do |pigeon_info_category_hash, pigeon_info_hash|
- 
-
   pigeon_info_hash.each do |pigeon_quality, pigeon_names_array|
     pigeon_names_array.each do |name|
       if !pigeon_list[name]
