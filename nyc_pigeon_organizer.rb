91e6e1b28df3866749a4251d71ef017bee9dc012
Ciara Picou <ciarapicou@att.net>
2020-07-28 19:57:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+
+
+
+# to create a new hash from an old one using map
+# hash = {a: 2, b: 3}
+# hash.map { |k, v| [k, v + 3] }.to_h     # => { a => 5, b => 6 }
+
+
+# to create a new hash from an old one using each_with_object
+# hash = { a: 2, b: 3 }
+# hash.each_with_object({}) do |(k, v), a|
+# a[k] = v + 3
+# end                   # => { a => 5, b => 6 }
+
 # The viedo solution explained:
 # def nyc_pigeon_organizer(data)
 #   final_results = data.each_with_object({}) do |(key, value), final_array|
@@ -27,35 +41,6 @@
   
 # end
 
-#   my original attempt: 
-
-$pigeon_array = []
-$pigeon_list = Hash.new
-
-def create_pigeon_name_hash(data)
-data.each do |pigeon_info_hash , pigeon_info_details_hash|
-  
-    pigeon_info_details_hash.each do|info_key , value_array|
-      value_array.each do |pigeon_name|
-          $pigeon_array.push(pigeon_name)
-    end
-  end
-end
-$pigeon_array.uniq!
-$pigeon_list = Hash[$pigeon_array.collect{|item| [item, {} ]}]
-$pigeon_list
-
-end
 
 
 
-# to create a new hash from an old one using map
-# hash = {a: 2, b: 3}
-# hash.map { |k, v| [k, v + 3] }.to_h     # => { a => 5, b => 6 }
-
-
-# to create a new hash from an old one using each_with_object
-# hash = { a: 2, b: 3 }
-# hash.each_with_object({}) do |(k, v), a|
-# a[k] = v + 3
-# end                   # => { a => 5, b => 6 }
