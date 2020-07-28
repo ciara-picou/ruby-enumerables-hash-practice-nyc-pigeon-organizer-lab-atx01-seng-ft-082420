883dab6a2b5f08dbbec5cb940c2effc488a3dd36
Ciara Picou <ciarapicou@att.net>
2020-07-28 19:58:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require "pry"
+  
+$pigeon_array = []
+$pigeon_list = Hash.new
+
+def create_pigeon_name_hash(data)
+data.each do |pigeon_info_hash , pigeon_info_details_hash|
+  
+    pigeon_info_details_hash.each do|info_key , value_array|
+      value_array.each do |pigeon_name|
+          $pigeon_array.push(pigeon_name)
+     end
+  end
+end
+$pigeon_array.uniq!
+$pigeon_list = Hash[$pigeon_array.collect{|item| [item, {} ]}]
+$pigeon_list
+
+end
+
+def nyc_pigeon_organizer(data)
+  create_pigeon_name_hash(data)
+  $pigeon_list.map do |lil_pidgies,values|
+    values_hash = Hash[data.keys.collect { |values| [values, {}] } ]
+    binding.pry
+# lil_pidgies.each_with_object)({}) do ||
+  end
+  $pigeon_list
+end
 
 
 
