92f8056b9f706a7c4c9647b6b1e25a517a4e2326
Ciara Picou <ciarapicou@att.net>
2020-07-28 18:13:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,31 +1,27 @@
 
-  require "pry"
-$pigeon_array = []
-$pigeon_list = Hash.new
+#   require "pry"
+# $pigeon_array = []
+# $pigeon_list = Hash.new
 
-def create_pigeon_name_hash(data)
-data.each do |pigeon_info_hash , pigeon_info_details_hash|
+# def create_pigeon_name_hash(data)
+# data.each do |pigeon_info_hash , pigeon_info_details_hash|
   
-    pigeon_info_details_hash.each do|info_key , value_array|
-      value_array.each do |pigeon_name|
-          $pigeon_array.push(pigeon_name)
-     end
-  end
-end
-$pigeon_array.uniq!
-$pigeon_list = Hash[$pigeon_array.collect{|item| [item, {} ]}]
-$pigeon_list
+#     pigeon_info_details_hash.each do|info_key , value_array|
+#       value_array.each do |pigeon_name|
+#           $pigeon_array.push(pigeon_name)
+#     end
+#   end
+# end
+# $pigeon_array.uniq!
+# $pigeon_list = Hash[$pigeon_array.collect{|item| [item, {} ]}]
+# $pigeon_list
 
-end
+# end
 
-def nyc_pigeon_organizer(data)
-  create_pigeon_name_hash(data)
-  $pigeon_list.map do |lil_pidgies,values|
-    binding.pry
-    # lil_pidgies.each_with_object)({}) do ||
-  end
-  $pigeon_list
-end
+# def nyc_pigeon_organizer(data)
+#   create_pigeon_name_hash(data)
+#   $pigeon_list
+# end
 # hash = {a: 2, b: 3}
 # hash.map { |k, v| [k, v + 3] }.to_h     # => { a => 5, b => 6 }
 
